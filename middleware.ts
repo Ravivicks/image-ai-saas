24c0712b45138cb3c7939ed6ab5cc50be80afd92
Ravi Anuragi <anuragiravi85@gmail.com>
2024-07-13 09:46:47 +0530

    final

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/api/webhooks/clerk"]);
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
