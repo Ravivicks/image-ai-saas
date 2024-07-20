3214ff6cfb2593c84c06824cf8f5e4deaf96bb8b
Ravi Anuragi <anuragiravi85@gmail.com>
2024-07-20 11:28:23 +0530

    change in user route

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,10 +64,10 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
-      photo: image_url,
+      username: username || "",
+      firstName: first_name || "",
+      lastName: last_name || "",
+      photo: image_url as string,
     };
 
     const newUser = await createUser(user);
