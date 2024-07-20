a8c65992bfff9f493bf9ae278942d8a732aceb1b
Ravi Anuragi <anuragiravi85@gmail.com>
2024-07-20 16:44:05 +0530

    middleware changes again

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)", "/"],
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+  "/",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
